5a8eddd1b98c7d67a62cba0a416074e217fb39e7
Topi <111167233+AriSophDev@users.noreply.github.com>
2026-07-30 14:33:24 -0500

    Initial x86_64 kernel boot setup

--- boot/boot.asm
+++ boot/boot.asm
@@ -2,24 +2,30 @@ section .multiboot
 align 8
 
 mb2_header:
-    dd 0xE85250D6          ; Magic
-    dd 0                   ; Architecture (0 = i386/x86)
+    dd 0xE85250D6
+    dd 0
     dd mb2_end - mb2_header
     dd -(0xE85250D6 + 0 + (mb2_end - mb2_header))
 
-    dw 0                   ; End tag type
-    dw 0                   ; Flags
-    dd 8                   ; Size
+    dw 0
+    dw 0
+    dd 8
+mb2_end
 
-mb2_end:
+section .bss
+align 16
+stack_bottom:
+    resb 16384
+stack_top:
 
-
+section .text
 global _start
 extern kernel_main
 
-section .text
-
 _start:
+    ; GRUB entra en 32 bits, así que usa ESP aquí
+    mov esp, stack_top
+
     call kernel_main
 
 .hang:
--- kernel/kernel.cpp
+++ kernel/kernel.cpp
@@ -1,15 +1,12 @@
-#include "vga.hpp"
+#include <stdint.h>
 
 extern "C" void kernel_main() {
+    volatile uint16_t* vga = (volatile uint16_t*)0xB8000;
 
-    vga::clear_screen();
-
-    vga::set_color(0x0A);
-    vga::put_line("hola");
-
-    vga::set_color(0x0F);
-    vga::put_line("Welcome to TopiOS");
+    vga[0] = 0x0F48; // H
+    vga[1] = 0x0F69; // i
 
     while (true) {
+        asm volatile("hlt");
     }
 }
